a1c6f24a37e44f3084aee0f0f61441ccbae5b7c8
RanhaoKang <ranhaokang@gmail.com>
2026-01-04 13:34:27 +0800

    m

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,6 +71,7 @@ vim.pack.add {
         { src = 'https://github.com/junegunn/fzf' },
     { src = 'https://github.com/sindrets/diffview.nvim' },
     { src = 'https://github.com/blazkowolf/gruber-darker.nvim' },
+    { src = 'https://github.com/tpope/vim-surround' },
 }
 vim.cmd.colorscheme 'gruber-darker'
 require('Comment').setup()

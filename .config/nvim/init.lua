8a9d29400e87fd7bf82aec4874888ee907b35ec5
RanhaoKang <ranhaokang@gmail.com>
2026-01-09 02:20:29 +0800

    commit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,6 +71,8 @@ vim.pack.add {
     { src = 'https://github.com/blazkowolf/gruber-darker.nvim' },
     { src = 'https://github.com/nvim-mini/mini.surround' },
     { src = 'https://github.com/stevearc/oil.nvim' },
+    { src = 'https://github.com/NeogitOrg/neogit' },
+    { src = 'https://github.com/nvim-lua/plenary.nvim' },
 }
 
 if not vim.env.EINK then
